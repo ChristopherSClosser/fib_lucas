6d73d354d68021455452e4afdb94ae0d416b26f0
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-10-17 16:52:56 -0400

    started work on fibonacci test

--- test_fib.py
+++ test_fib.py
@@ -1,1 +1,9 @@
 """Test for fibinachi sequence."""
+
+
+def test_fibonacci(fib_sequence):
+    """Test fibonacci list for fibonacci pattern."""
+    from fib import fibonacci
+    for i in fib_sequence:
+        assert fibonacci((fib_sequence[i]) + (fib_sequence[i + 1])) \
+            == fib_sequence[i + 2]
