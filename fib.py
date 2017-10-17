9839beb5a5752f0f5bbf972d47d0ff24fd974de0
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-10-17 18:52:10 -0400

    refactored code for assignment parameters.

--- fib.py
+++ fib.py
@@ -1,16 +1,20 @@
 """Fibinachi sequence."""
 
 
-fib_list = []
+
+# def fibonacci(n):
+#     """Fibinachi sequence function."""
+#     fib_list.append(n)
+#     fib_list.append(n + 1)
+#     for i in range(10):
+#         # print(fib_list)
+#         fib_list.append(fib_list[i] + fib_list[i + 1])
+#     # for i in fib_list:
+#     #     print(fib_list[i])
+#     return fib_list
 
 
-def fibonacci(n):
-    """Fibinachi sequence function."""
-    fib_list.append(n)
-    fib_list.append(n + 1)
-    for i in range(10):
-        # print(fib_list)
-        fib_list.append(fib_list[i] + fib_list[i + 1])
-    # for i in fib_list:
-    #     print(fib_list[i])
-    return fib_list
+def fibinacci(n):
+	"""Returns value at index of n."""
+    fib_list = [0, 1, 1, 2, 3, 5, 8, 13]
+    return fib_list[n]
--- test_lucas.py
+++ test_lucas.py
@@ -10,3 +10,10 @@
 #         assert fibonacci((fib_list[index]) + (fib_list[index + 1])) \
 #             == fib_list[index + 2]
 #         index += 1
+
+lucas_correct_list = []
+def test_lucas():
+    """Test lucas list for lucas pattern."""
+    for i in range(len(lucas_list)):
+        print(lucas_list[i], lucas_correct_list[i])
+        assert lucas_list[i] is lucas_correct_list[i]
