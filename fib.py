229f5a9d45dccaa91079c2dba360fef13e97d96d
Chris Closser <chrisclosser1@gmail.com>
2017-10-17 14:03:51 -0700

    fibonacci function

--- fib.py
+++ fib.py
@@ -1,1 +1,10 @@
 """Fibinachi sequence."""
+
+
+def fibonacci(n):
+    """Fibinachi sequence function."""
+    fib_list = []
+    fib_list.append(n)
+    fib_list.append(n + 1)
+    for i in range(10):
+        fib_list.append(fib_list[i] + fib_list[i + 1])
